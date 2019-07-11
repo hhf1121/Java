d7c4d8b922e34a5a0bdd69c891888ee3e20098b1
hhf <290071186@qq.com>
2019-07-11 20:09:18 +0800

    spring注解方式bean

--- JavaBase/src/main/java/Spring/SpringApplicarion.java
+++ JavaBase/src/main/java/Spring/SpringApplicarion.java
@@ -1,6 +1,8 @@
 package Spring;
 
+import Spring.ConfigBean.Config;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import Spring.Bean.Person;
@@ -8,9 +10,12 @@ import Spring.Bean.Person;
 public class SpringApplicarion {
 
 	public static void main(String[] args) {
-		
-		ApplicationContext  ctx=new ClassPathXmlApplicationContext("bean.xml");
-		Person bean = (Person)ctx.getBean("personId");
+
+		//创建ioc容器，用xml的方式
+//		ApplicationContext  ctx=new ClassPathXmlApplicationContext("bean.xml");
+		//创建ioc容器，用注解的方式
+		AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(Config.class);
+		Person bean = (Person)ctx.getBean("person");
 		System.out.println(bean.getName()+"-"+bean.getAge());
 	}
 	
