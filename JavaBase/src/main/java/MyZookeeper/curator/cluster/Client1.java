b30a3d56a4d7daee90c01217cb16cb7041a834b9
hehf <hehf@yimidida.com>
2019-10-16 16:47:03 +0800

    zk

---
+++ JavaBase/src/main/java/MyZookeeper/curator/cluster/Client1.java
@@ -0,0 +1,12 @@
+package MyZookeeper.curator.cluster;
+
+
+public class Client1 {
+
+	public static void main(String[] args) throws Exception{
+		
+		CuratorWatcher watcher = new CuratorWatcher();
+		System.out.println("c1,start...");
+		Thread.sleep(100000000);
+	}
+}
---
+++ JavaBase/src/main/java/MyZookeeper/curator/cluster/Client2.java
@@ -0,0 +1,11 @@
+package MyZookeeper.curator.cluster;
+
+public class Client2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		CuratorWatcher watcher = new CuratorWatcher();
+		System.out.println("c2,start...");
+		Thread.sleep(100000000);
+	}
+}
