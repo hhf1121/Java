9164c1d72881edf10ac4394361f1521a690ba091
hehf <hehf@yimidida.com>
2020-06-29 13:50:34 +0800

    jdk代理对象

--- JavaBase/src/main/java/Spring/Aop/InvokeTest.java
+++ JavaBase/src/main/java/Spring/Aop/InvokeTest.java
@@ -1,12 +1,24 @@
 package Spring.Aop;
 
 
+import com.google.common.collect.Lists;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InvokeTest {
+
+    volatile List<String>  lists= Lists.newArrayList();
+    @Before
+    public void Before(){
+        for (int i = 0; i < 100; i++) {
+            lists.add(""+i);
+        }
+    }
 
 
     @Test
@@ -18,5 +30,29 @@ public class InvokeTest {
         IMyGetData o = (IMyGetData) Proxy.newProxyInstance(classLoader, classes, myGetData);
         System.out.println(o.getData("我是入参"));
     }
+
+
+    /**
+     * 并发操作，导致遍历的时候，报了错。
+     * @throws InterruptedException
+     */
+    @Test
+    public void test1() throws InterruptedException {
+        new Thread(() -> {
+            while (true){
+                lists.forEach(o -> {
+                    System.out.println(o);
+                });
+            }
+        }).start();
+        Thread.sleep(1000);
+        new Thread(() -> {
+//            ArrayList<String> list = Lists.newArrayList("A","B","C");
+//            lists.addAll(list);
+            lists.remove("1");
+        }).start();
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
 
 }
